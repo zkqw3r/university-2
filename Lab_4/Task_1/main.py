7544a2b8a77910cc2d7ee5e35f7d2782e14f677e
zkqw3r <117681609+zkqw3r@users.noreply.github.com>
2025-10-23 00:17:35 +0300

    Refactor find_max to include array name in output

--- Lab_4/Task_1/main.py
+++ Lab_4/Task_1/main.py
@@ -1,24 +1,26 @@
 def find_max(array1, array2):
     mx = array1[0]
     idx = 0
-    mx_array = array1
     for i in range(len(array1)):
         if array1[i] > mx:
             mx = array1[i]
             idx = i
+            mx_array_name = "A"
+            mx_array = array1
     for i in range(len(array2)):
         if array2[i] > mx:
             mx = array2[i]
             idx = i
+            mx_array_name = "B"
             mx_array = array2
-    return mx, idx+1, mx_array
+    return mx, idx+1, mx_array, mx_array_name
     
 
 def main():
-    a = [int(input(f"a[{i}]: ")) for i in range(int(input("Введите длину массива а: ")))]
-    b = [int(input(f"b[{i}]: ")) for i in range(int(input("Введите длину массива b: ")))]
-    mx, index_mx, mx_array = find_max(a, b)
-    print(f"\nМассив с максимальным значением: {mx_array}\n"+
+    a = [int(input(f"Введите элемент a[{i}]: ")) for i in range(int(input("Введите длину массива а: ")))]
+    b = [int(input(f"Введите элемент b[{i}]: ")) for i in range(int(input("Введите длину массива b: ")))]
+    mx, index_mx, mx_array, mx_array_name = find_max(a, b)
+    print(f"\nМассив с максимальным значением - {mx_array_name}: {mx_array}\n"+
         f"Максимальное значение: {mx}\n"+
         f"Порядковый номер: {index_mx}")
     
