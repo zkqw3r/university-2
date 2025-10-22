adf0f3de558c384dfa23f46f9f9517501cd81341
zkqw3r <117681609+zkqw3r@users.noreply.github.com>
2025-10-23 00:17:11 +0300

    Update main.py

--- Lab_4/Task_2/main.py
+++ Lab_4/Task_2/main.py
@@ -1,14 +1,16 @@
 def find_max(set1, set2):
     diff = set1-set2
+    print(f"\nРазность множеств: {diff}\n")
     return max(diff)
 
 
 def main():
     a, b = set(), set()
     for i in range(int(input("Введите длину множества a: "))):
-        a.add(int(input(f'a[{i}]: ')))
+        a.add(int(input(f'Введите элемент a[{i}]: ')))
     for i in range(int(input("Введите длину множества b: "))):
-        b.add(int(input(f'b[{i}]: ')))
+        b.add(int(input(f'Введите элемент b[{i}]: ')))
+    print(f"Максимальный элемент: {find_max(a, b)}")
 
         
 if __name__ == "__main__":
